03d33e18bee59f5aa50b8b9bd78a8e40188cca4c
Leonardo Alt <leo@ethereum.org>
2018-11-07 17:21:08 +0100

    Add Devcon talk

---
+++ smtchecker_devcon_iv/example_false_positive_fixed.sol
@@ -0,0 +1,31 @@
+pragma solidity >0.4.25;
+pragma experimental SMTChecker;
+
+contract IFace
+{
+    function f() external;
+}
+
+contract C
+{
+    IFace externalContract;
+	uint sum;
+	uint count;
+
+	function () external payable {
+		require(msg.value > 0);
+		require(sum + msg.value <= 1000000);
+		sum = sum + msg.value;
+		count = count + 1;
+	}
+
+    function average() public returns (uint avg) {
+		require(count > 0);
+        externalContract.f();
+		require(count > 0);
+		require(sum >= count);
+		avg = sum / count;
+		assert(avg > 0);
+		return avg;
+    }
+}
---
+++ smtchecker_devcon_iv/example_false_positive_simple.sol
@@ -0,0 +1,22 @@
+pragma solidity >0.4.25;
+pragma experimental SMTChecker;
+
+contract C
+{
+	uint sum;
+	uint count;
+
+	function () external payable {
+		require(msg.value > 0);
+		require(sum + msg.value <= 1000000);
+		sum = sum + msg.value;
+		count = count + 1;
+	}
+
+    function average() public view returns (uint avg) {
+		require(count > 0);
+		avg = sum / count;
+		assert(avg > 0);
+		return avg;
+    }
+}
